7c74df7f5ee298c85852ecb19759cf50fc673bc5
Dakota <Dakotawhittle1203@gmail.com>
2020-05-20 14:32:19 -0400

    added tests

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class fizzBuzz
+    {
+        public string Get(int num)
+        {
+            if (num == 3)
+            {
+                return "Fizz"; 
+
+            }
+            return num.ToString();
+        }
+
+    }
+}
