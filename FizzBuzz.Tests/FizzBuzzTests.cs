02aee1d3efae5304f050c2d9c728361a186737f0
Dakota <Dakotawhittle1203@gmail.com>
2020-05-21 10:31:03 -0400

    added tests and code for multiples of 3 and 5

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -8,7 +8,7 @@ namespace FizzBuzz.Tests
         [Fact]
         public void Get_returns_1_for_1()
         {
-            var fizzBuzz = new fizzBuzz();
+            var fizzBuzz = new FizzBuzz();
             var result = fizzBuzz.Get(1);
             Assert.Equal("1", result);
         }
@@ -16,16 +16,37 @@ namespace FizzBuzz.Tests
         [Fact]
         public void Get_returns_2_for_2()
         {
-            var fizzBuzz = new fizzBuzz();
+            var fizzBuzz = new FizzBuzz();
             var result = fizzBuzz.Get(2);
             Assert.Equal("2", result);
         }
         [Fact]
         public void Get_returns_Fizz_for_3()
         {
-            var fizzBuzz = new fizzBuzz();
+            var fizzBuzz = new FizzBuzz();
             var result = fizzBuzz.Get(3);
             Assert.Equal("Fizz", result);
+        }
+        [Fact]
+        public void Get_returns_Fizz_for_6()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Get(6);
+            Assert.Equal("Fizz", result);
+        }
+        [Fact]
+        public void Get_Returns_Buzz_For_5()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Get(5);
+            Assert.Equal("Buzz", result);
+        }
+        [Fact]
+        public void Get_Returns_Buzz_For_10()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Get(10);
+            Assert.Equal("Buzz", result);
         }
     }
 }
--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,17 +4,28 @@ using System.Text;
 
 namespace FizzBuzz
 {
-    public class fizzBuzz
+    public class FizzBuzz
     {
         public string Get(int num)
         {
-            if (num == 3)
+            if (IsMultipleOf3(num))
             {
-                return "Fizz"; 
-
+                return "Fizz";
+            }
+            if (IsMultipleOf10(num))
+            {
+                return "Buzz";
             }
             return num.ToString();
         }
 
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf10(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
     }
 }
